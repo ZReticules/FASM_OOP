bed7bf2d033e9214e4639b828f6d6429aefb945e
unknown <ZReticules>
2025-01-12 22:17:58 +0300

    Multiarchitecture version of FASM_OOP

--- x64/WINUSER/DialogForm/DialogForm.asm
+++ x64/WINUSER/DialogForm/DialogForm.asm
@@ -136,21 +136,6 @@ proc DIALOGFORM_WM_CLOSE_NOMODAL, lpForm, lpParam
 	@jret [PostQuitMessage](0)
 endp
 
-proc DIALOGFORM_WM_CTLCOLOR uses rbx rsi, formLp, paramsLp
-	virtObj .params:arg params at rbx
-	mov rbx, rdx
-	@call [GetWindowLongPtrA]([.params.lparam], GWL_USERDATA)
-	test rax, rax
-	jz .noVal
-		virtObj .control:arg _DLG_SPECIAL_CONTROL at rsi
-		mov rsi, rax
-		@call [SetBkColor]([.params.wparam], [.control.bkColor])
-		@call [SetTextColor]([.params.wparam], [.control.txColor])
-		mov rax, [.control.bgColorBrush]
-	.noVal:
-	ret
-endp
-
 ; proc DIALOGFORM_WM_CTLCOLORDLG, formLp, paramsLp
 ; 	virtObj .params:arg params
 ; 	@call [GetWindowLongPtrA]([.params.lparam], GWL_USERDATA)
